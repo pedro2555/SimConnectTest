33801f5591c099509f79114f841ce29c599faecf
Tiago <tmavicente@gmail.com>
2018-05-28 20:57:21 +0100

    httprequest

--- PilotClient/connectedExampleFrm.Designer.cs
+++ PilotClient/connectedExampleFrm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtLog
@@ -38,15 +39,26 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtLog.Location = new System.Drawing.Point(12, 12);
             this.txtLog.Name = "txtLog";
-            this.txtLog.Size = new System.Drawing.Size(253, 389);
+            this.txtLog.Size = new System.Drawing.Size(253, 351);
             this.txtLog.TabIndex = 0;
             this.txtLog.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.connectedExampleFrm_SimConnectOpen);
             // 
             // connectedExampleFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(277, 413);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtLog);
             this.Name = "connectedExampleFrm";
             this.Text = "Form1";
@@ -59,6 +71,7 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- PilotClient/connectedExampleFrm.cs
+++ PilotClient/connectedExampleFrm.cs
@@ -1,10 +1,17 @@
 ﻿using SimLib;
 using System;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Net.Http;
 
 namespace PilotClient
-{
+{   
+
     public partial class connectedExampleFrm : SimConnectForm
     {
+        private static readonly HttpClient client = new HttpClient();
+
+        private HttpRequestMessage message;
         // Response number 
         int response = 1;
 
@@ -31,6 +38,14 @@ namespace PilotClient
         private void connectedExampleFrm_SimConnectOpen(object sender, EventArgs e)
         {
             displayText("Connected to simulator");
+
+            Process.Start("http://37.59.115.154/html/login.html");
+            
+        }
+
+        private void requestLoginSquawk()
+        {
+            message = new HttpRequestMessage(HttpMethod.Get, "https://49ce63db-126a-45c8-9493-04dd5206674f.mock.pstmn.io/login");
         }
 
         private void connectedExampleFrm_SimConnectClosed(object sender, EventArgs e)
