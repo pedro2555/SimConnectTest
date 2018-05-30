3a8fe0c7ce7d520114d96b4976473b1b992d3774
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-05-30 08:18:45 +0100

    SimConnectPositionChanged event

--- SimLib/SimConnectForm.cs
+++ SimLib/SimConnectForm.cs
@@ -65,6 +65,7 @@ namespace SimLib
 
                     RegisterEvents();
                     RegisterDataDefinitions();
+                    RegisterPosition();
                 }
                 catch (COMException)
                 {
