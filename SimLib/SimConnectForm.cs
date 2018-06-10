600e281cf2811924c76b7ccc4294c12645bba2f1
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-06-10 10:23:56 +0100

    major clean
    moved Radios struct to class and into SimObjectType framework

--- SimLib/SimConnectForm.cs
+++ SimLib/SimConnectForm.cs
@@ -63,7 +63,41 @@ namespace SimLib
                     // the constructor is similar to SimConnect_Open in the native API 
                     SimConnectWrapper.Sim = new SimConnect("SimLib.SimLibSimConnect", Handle, WM_USER_SIMCONNECT, null, 0);
 
-                    RegisterDataDefinitions();
+                    // Register data definitions
+                    SimObjectType<Position>.Register(new SimObjectType<Position>.Field[]
+                    {
+                        new SimObjectType<Position>.Field()
+                        { DatumName = "Title", UnitsName = null,
+                            DatumType = SIMCONNECT_DATATYPE.STRING256 },
+                        new SimObjectType<Position>.Field()
+                        { DatumName = "PLANE LATITUDE", UnitsName = "degrees",
+                            DatumType = SIMCONNECT_DATATYPE.FLOAT64 },
+                        new SimObjectType<Position>.Field()
+                        { DatumName = "PLANE LONGITUDE", UnitsName = "degrees",
+                            DatumType = SIMCONNECT_DATATYPE.FLOAT64 },
+                        new SimObjectType<Position>.Field()
+                        { DatumName = "PLANE ALTITUDE", UnitsName = "feet",
+                            DatumType = SIMCONNECT_DATATYPE.FLOAT64 },
+                        new SimObjectType<Position>.Field()
+                        { DatumName = "PLANE PITCH DEGREES", UnitsName = "degrees",
+                            DatumType = SIMCONNECT_DATATYPE.FLOAT64 },
+                        new SimObjectType<Position>.Field()
+                        { DatumName = "PLANE BANK DEGREES", UnitsName = "degrees",
+                            DatumType = SIMCONNECT_DATATYPE.FLOAT64 },
+                        new SimObjectType<Position>.Field()
+                        { DatumName = "PLANE HEADING DEGREES TRUE", UnitsName = "degrees",
+                            DatumType = SIMCONNECT_DATATYPE.FLOAT64 },
+                        new SimObjectType<Position>.Field()
+                        { DatumName = "AIRSPEED TRUE", UnitsName = "knots",
+                            DatumType = SIMCONNECT_DATATYPE.FLOAT64 },
+                    });
+
+                    SimObjectType<Radios>.Register(new SimObjectType<Radios>.Field[]
+                    {
+                        new SimObjectType<Radios>.Field()
+                        { DatumName = "TRANSPONDER CODE:1", UnitsName = "BCO16",
+                            DatumType = SIMCONNECT_DATATYPE.INT32 },
+                    });
 
                     //RegisterEvents();
                 }
@@ -140,6 +174,11 @@ namespace SimLib
                     await Task.Delay(SimConnectPoolCooldown);
                 }
             }
+        }
+
+        private void RegisterDataDefinitions()
+        {
+
         }
     }
 }
